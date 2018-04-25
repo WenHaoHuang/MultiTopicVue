c3b93d3de8ddbc16f86778f7d549059735897406
huangwenhao <huangwenhao@kesucorp.com>
2018-04-25 15:47:14 +0800

    微调

--- build/utils.js
+++ build/utils.js
@@ -75,15 +75,13 @@ exports.createNotifierCallback = () => {
 
     return (severity, errors) => {
         if (severity !== 'error') return
-        console.log(errors)
         const error = errors[0]
         const filename = error.file && error.file.split('!').pop()
 
         notifier.notify({
             title: process.env.npm_package_name,
             message: severity + ': ' + error.name,
-            subtitle: filename || '',
-            icon: path.join(__dirname, 'logo.png')
+            subtitle: filename || ''
         })
     }
 }
